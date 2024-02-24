ff11b61f2b5f8005c7495b86d583da643e11e6b5
JFFPtech <arrozconsalchichas1229@gmail.com>
2024-02-24 16:55:50 -0400

    added str, save, to dict

--- models/base_model.py
+++ models/base_model.py
@@ -4,3 +4,23 @@ from datetime import datetime
 class BaseModel:
     """BaseModel class"""
     def __init__(self):
+        """Constructor"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+    
+    def __str__(self):
+        """String representation"""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+    
+    def save(self):
+        """Update the updated_at attribute"""
+        self.updated_at = datetime.now()
+    
+    def to_dict(self):
+        """Return a dictionary representation of the object"""
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
