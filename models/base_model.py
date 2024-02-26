d32720366d3b3bcf143570fc0bac41b8c96fdd6a
JFFPtech <arrozconsalchichas1229@gmail.com>
2024-02-26 11:50:16 -0400

    rollback for checkers

--- models/base_model.py
+++ models/base_model.py
@@ -1,35 +1,32 @@
 import uuid
 from datetime import datetime
-from models import storage
 
 class BaseModel:
-    """BaseModel class"""
     def __init__(self, *args, **kwargs):
-        """Constructor method"""
+        """Initialize BaseModel instance."""
         if kwargs:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
-                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                if key != '__class__':
+                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                elif key != '__class__':
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-    
+
     def __str__(self):
-        """String representation"""
+        """Return string representation of BaseModel."""
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
-    
+
     def save(self):
-        """Update the updated_at attribute"""
+        """Update updated_at with current datetime."""
         self.updated_at = datetime.now()
-        storage.save()
-    
+
     def to_dict(self):
-        """Returns a dictionary representation of the object"""
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
-        return obj_dict
+        """Return dictionary representation of BaseModel."""
+        model_dict = self.__dict__.copy()
+        model_dict['__class__'] = self.__class__.__name__
+        model_dict['created_at'] = self.created_at.isoformat()
+        model_dict['updated_at'] = self.updated_at.isoformat()
+        return model_dict
