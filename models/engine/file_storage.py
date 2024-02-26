de2e269f8c54c4bf4703d52b1649b4a8b8deae35
JFFPtech <arrozconsalchichas1229@gmail.com>
2024-02-26 12:53:08 -0400

    holy moley the json file isnt empty

---
+++ models/engine/file_storage.py
@@ -0,0 +1,32 @@
+import json
+
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        return self.__objects
+
+    def new(self, obj):
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        serialized_objects = {}
+        for key, value in FileStorage.__objects.items():
+            serialized_objects[key] = value.to_dict()
+        with open(FileStorage.__file_path, 'w') as file:
+            json.dump(serialized_objects, file)
+
+    def reload(self):
+        try:
+            with open(FileStorage.__file_path, 'r') as file:
+                data = json.load(file)
+                for key, value in data.items():
+                    class_name, obj_id = key.split('.')
+                    module = __import__("models.{}".format(class_name), fromlist=[class_name])
+                    class_ = getattr(module, class_name)
+                    obj = class_(**value)
+                    FileStorage.__objects[key] = obj
+        except FileNotFoundError:
+            pass
