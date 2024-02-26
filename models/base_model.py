c8e289014112f1a7524e543764a8907856c81d8d
JFFPtech <arrozconsalchichas1229@gmail.com>
2024-02-26 11:53:47 -0400

    changes

--- models/base_model.py
+++ models/base_model.py
@@ -2,31 +2,34 @@ import uuid
 from datetime import datetime
 
 class BaseModel:
-    def __init__(self, *args, **kwargs):
-        """Initialize BaseModel instance."""
+    def __init__(self, **kwargs):
         if kwargs:
-            for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
-                elif key != '__class__':
-                    setattr(self, key, value)
+            if 'id' in kwargs:
+                self.id = kwargs['id']
+            else:
+                self.id = str(uuid.uuid4())
+            if 'created_at' in kwargs:
+                self.created_at = datetime.fromisoformat(kwargs['created_at'])
+            else:
+                self.created_at = datetime.now()
+            if 'updated_at' in kwargs:
+                self.updated_at = datetime.fromisoformat(kwargs['updated_at'])
+            else:
+                self.updated_at = datetime.now()
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
 
     def __str__(self):
-        """Return string representation of BaseModel."""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """Update updated_at with current datetime."""
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """Return dictionary representation of BaseModel."""
-        model_dict = self.__dict__.copy()
-        model_dict['__class__'] = self.__class__.__name__
-        model_dict['created_at'] = self.created_at.isoformat()
-        model_dict['updated_at'] = self.updated_at.isoformat()
-        return model_dict
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = type(self).__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
