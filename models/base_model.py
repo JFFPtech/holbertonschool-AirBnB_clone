f54015e28459e70966e0d861f6ea4b2945e5edfb
JFFPtech <arrozconsalchichas1229@gmail.com>
2024-02-26 09:54:51 -0400

    added kwargs to base, tested

--- models/base_model.py
+++ models/base_model.py
@@ -3,11 +3,18 @@ from datetime import datetime
 
 class BaseModel:
     """BaseModel class"""
-    def __init__(self):
-        """Constructor"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        """Constructor method"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
     
     def __str__(self):
         """String representation"""
@@ -18,7 +25,7 @@ class BaseModel:
         self.updated_at = datetime.now()
     
     def to_dict(self):
-        """Return a dictionary representation of the object"""
+        """Returns a dictionary representation of the object"""
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
