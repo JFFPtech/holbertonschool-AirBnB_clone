db62d944fe840499544e990c82782b3871284e04
JFFPtech <arrozconsalchichas1229@gmail.com>
2024-02-26 12:03:13 -0400

    added changes

--- models/base_model.py
+++ models/base_model.py
@@ -2,34 +2,20 @@ import uuid
 from datetime import datetime
 
 class BaseModel:
-    def __init__(self, **kwargs):
-        if kwargs:
-            if 'id' in kwargs:
-                self.id = kwargs['id']
-            else:
-                self.id = str(uuid.uuid4())
-            if 'created_at' in kwargs:
-                self.created_at = datetime.fromisoformat(kwargs['created_at'])
-            else:
-                self.created_at = datetime.now()
-            if 'updated_at' in kwargs:
-                self.updated_at = datetime.fromisoformat(kwargs['updated_at'])
-            else:
-                self.updated_at = datetime.now()
-        else:
+    def __init__(self):
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
 
     def __str__(self):
         return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
-
+    
     def save(self):
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = type(self).__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
-        return obj_dict
+        dict_obj = dict(self.__dict__)
+        dict_obj['__class__'] = type(self).__name__
+        dict_obj['created_at'] = self.created_at.isoformat()
+        dict_obj['updated_at'] = self.updated_at.isoformat()
+        return dict_obj
