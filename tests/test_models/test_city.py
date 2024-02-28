0492dd3a949ccc8706d6404bfeee73eb036e8639
JFFPtech <arrozconsalchichas1229@gmail.com>
2024-02-28 19:08:24 -0400

    test for city class

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,45 +1,47 @@
 #!/usr/bin/python3
-"""Module test_city"""
+"""
+Unittests for base model class
+"""
 import unittest
 from models.city import City
 from models.base_model import BaseModel
+from datetime import datetime
+from uuid import UUID
+from models import storage
 
 
-class TestCity(unittest.TestCase):
-    """Test for the City class"""
+class TestsCity(unittest.TestCase):
+    
+    obj = City()
 
-    def setup(self):
-        """Set up"""
-        self.c1 = City()
+    def setUp(self):
+        """set initial"""
+        name = ""
+        state_id = ""
 
-    def test_name(self):
-        """Testing City name"""
-        self.assertEqual(self.city.name, "")
+    def test_normal(self):
+        """normal cases"""
+        my_object = City()
+        my_object.name = "Westeros"
+        my_object.my_number = 29
+        my_object.save()
+        my_object_dict = my_object.to_dict()
+        self.assertEqual(my_object.name, "Westeros")
+        self.assertEqual(my_object.my_number, 29)
+        self.assertEqual(my_object.__class__.__name__, "City")
+        self.assertEqual(isinstance(my_object.created_at, datetime), True)
+        self.assertEqual(isinstance(my_object.updated_at, datetime), True)
+        self.assertEqual(type(my_object.__dict__), dict)
 
-    def test_state_id(self):
-        """Testing City state_id"""
-        self.assertEqual(self.city.state_id, "")
+    def test_subclass(self):
+        """test if class is subclass"""
+        self.assertEqual(issubclass(City, BaseModel), True)
+    
+    def test_type(self):
+        """test type of object"""
+        self.assertEqual(type(self.obj.name), str)
+        self.assertEqual(type(self.obj.state_id), str)
+	
 
-    def test_inheritance(self):
-        """Testing City inheritance"""
-        self.assertIsInstance(self.city, BaseModel)
-
-    def test_attributes(self):
-        """Testing City attributes"""
-        self.assertTrue(hasattr(self.city, "name"))
-        self.assertTrue(hasattr(self.city, "id"))
-        self.assertTrue(hasattr(self.city, "created_at"))
-        self.assertTrue(hasattr(self.city, "updated_at"))
-
-    def test_attribute_default(self):
-        """Test attribute default"""
-        self.assertEqual(self.city.name, "")
-        self.assertEqual(self.city.state_id, "")
-
-    def test_str(self):
-        """Testing City __str__"""
-        expected = "[City] ({}) {}".format(self.city.id, self.city.__dict__)
-        self.assertEqual(expected, str(self.city))
-
-    if __name__ == "__main__":
-        unittest.main()
+if __name__ == "__main__":
+    unittest.main()
