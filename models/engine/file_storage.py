ad29e470f78feb6dcb21a5e581f8281e10521661
JFFPtech <arrozconsalchichas1229@gmail.com>
2024-02-28 12:19:51 -0400

    added docs to funcs

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,6 +9,8 @@ from models.amenity import Amenity
 
 
 class FileStorage:
+    """This class represents a file storage system for objects in the AirBnB clone project."""
+
     __file_path = "file.json"
     __objects = {}
 
@@ -17,10 +19,12 @@ class FileStorage:
         return FileStorage.__objects
 
     def new(self, obj):
+        """Adds a new object to the __objects dictionary"""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         FileStorage.__objects[key] = obj
 
     def save(self):
+        """Serializes and saves the objects in the __objects dictionary to a JSON file"""
         serialized_objects = {}
         for key, value in FileStorage.__objects.items():
             serialized_objects[key] = value.to_dict()
@@ -28,6 +32,7 @@ class FileStorage:
             json.dump(serialized_objects, file)
 
     def reload(self):
+        """Deserializes and reloads the objects from the JSON file"""
         try:
             with open(FileStorage.__file_path, 'r') as file:
                 deserialized_objects = json.load(file)
